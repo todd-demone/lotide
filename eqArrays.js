09ae7682d81f4376e21eb13561ce370bd015f4c1
Todd Demone <tdemone1@gmail.com>
2021-10-22 14:51:22 +0000

    add eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑️🛑️🛑️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// function eqArrays
+// input => Array, Array
+// output => Boolean
+const eqArrays = function(arr1, arr2) {
+  // arrays are not identical if they have different lengths
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  // compare each item in arr1 to the corresponding item in arr2
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  // if all items are equal, return true
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
+
