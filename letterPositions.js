43e77d90d709c20e1719f588d313981194aa3e6c
Todd Demone <tdemone1@gmail.com>
2021-10-25 19:59:17 +0000

    add letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,63 @@
+// TEST FUNCTIONS
+
+// FXN eqArrays
+// input => Array, Array
+// output => Boolean
+const eqArrays = function(arr1, arr2) {
+  // arrays are not identical if they have different lengths
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  // compare each item in arr1 to the corresponding item in arr2
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  // if all items are equal, return true
+  return true;
+};
+
+// FXN assertArraysEqual
+// input => Array, Array
+// output => no return value; side effect (console.log())
+const assertArraysEqual = function(actual, expected) {
+  // Stringify the arrays so they look like arrays when printed to console.
+  const actualStr = JSON.stringify(actual);
+  const expectedStr = JSON.stringify(expected);
+  // compare actual and expected (both are arrays) to see if equal
+  // if result is true, print a pass message
+  if (eqArrays(actual, expected)) {
+    console.log(`✅️✅️✅️ Assertion Passed: ${actualStr} === ${expectedStr}`);
+  // else, print a fail message
+  } else {
+    console.log(`🛑️🛑️🛑️ Assertion Failed: ${actualStr} !== ${expectedStr}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const char = sentence[i];
+    if (char === ' ') {
+      continue;
+    }
+    if (results[char]) {
+      results[char].push(i);
+    } else {
+      results[char] = [i];
+    }
+  }
+  return results;
+};
+
+const sentence = 'lighthouse in the house';
+const result1 = letterPositions(sentence);
+console.log(result1);
+assertArraysEqual(result1['h'], [3, 5, 15, 18]);
+const result2 = letterPositions('hello');
+console.log(result2);
+assertArraysEqual(result2['h'], [0]);
+assertArraysEqual(result2['e'], [1]);
+assertArraysEqual(result2['l'], [2, 3]);
+assertArraysEqual(result2['o'], [4]);
