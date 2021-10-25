98f182313215c0301405679dd5f95b6c8b038941
Todd Demone <tdemone1@gmail.com>
2021-10-25 20:02:04 +0000

    add letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -53,10 +53,10 @@ const letterPositions = function(sentence) {
 
 const sentence = 'lighthouse in the house';
 const result1 = letterPositions(sentence);
-console.log(result1);
+// console.log(result1);
 assertArraysEqual(result1['h'], [3, 5, 15, 18]);
 const result2 = letterPositions('hello');
-console.log(result2);
+// console.log(result2);
 assertArraysEqual(result2['h'], [0]);
 assertArraysEqual(result2['e'], [1]);
 assertArraysEqual(result2['l'], [2, 3]);
