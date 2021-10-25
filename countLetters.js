0232a4a3662cc5155681a94d5493b412312eb7c9
Todd Demone <tdemone1@gmail.com>
2021-10-25 19:24:21 +0000

    add countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+// TEST FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑️🛑️🛑️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  // get rid of spaces
+  sentence = sentence.split(' ').join('');
+  // initialize the return object
+  let count = {};
+  for (const char of sentence) {
+    if (count[char]) {
+      count[char]++;
+    } else {
+      count[char] = 1;
+    }
+  }
+  return count;
+};
+
+const result1 = countLetters('this is a test');
+assertEqual(result1["t"], 3);
+assertEqual(result1['a'], 1);
+assertEqual(result1['z'], undefined);
+assertEqual(result1[' '], undefined);
+
+// {t: 3, h: 1, i: 2, s: 3, a: 1, e: 1}
