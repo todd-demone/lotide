25fbfe69a409f61bb6f3b57c64ce16bb1cd70a4e
Todd Demone <tdemone1@gmail.com>
2021-11-07 15:20:49 +0000

    add recursive call to eqArrays so function can compare nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -16,7 +16,11 @@ const eqArrays = function(arr1, arr2) {
   }
   // compare each item in arr1 to the corresponding item in arr2
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) { // recursive call
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
@@ -30,3 +34,42 @@ assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
 
+assertEqual(
+  eqArrays(
+    [[2, 3], [4]], 
+    [[2, 3], [4]]
+  ), 
+  true
+);
+
+assertEqual(
+  eqArrays(
+    [[2, 3], [4]],
+    [[2, 3], [4, 5]]
+  ),
+  false
+);
+
+assertEqual(
+  eqArrays(
+    [[2, 3], [4]],
+    [[2, 3], 4],
+  ),
+  false
+);
+
+assertEqual(
+  eqArrays(
+    [[2, 3], [4, [1, 2, [3, 4, 5, 6]]]], 
+    [[2, 3], [4, [1, 2, [3, 4, 5, 6]]]]
+  ), 
+  true
+);
+
+assertEqual(
+  eqArrays(
+    [[2, 3], [4, [1, 2, [3, 5, 5, 6]]]], 
+    [[2, 3], [4, [1, 2, [3, 4, 5, 6]]]]
+  ), 
+  false
+);
