0b44c12aa66384990ce36b3fa3e3bf1197161722
Todd Demone <tdemone1@gmail.com>
2021-11-08 18:33:07 +0000

    refactor to modularize assertArraysEqual.js and move test code to test/assertArraysEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,6 @@
-// FXN eqArrays
-// input => Array, Array
-// output => Boolean
-const eqArrays = function(arr1, arr2) {
-  // arrays are not identical if they have different lengths
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  // compare each item in arr1 to the corresponding item in arr2
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  // if all items are equal, return true
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
-// FXN assertArraysEqual
-// input => Array, Array
-// output => no return value; side effect (console.log())
+// Array, Array => no return value; side effect (console.log())
 const assertArraysEqual = function(actual, expected) {
   // Stringify the arrays so they look like arrays when printed to console.
   const actualStr = JSON.stringify(actual);
@@ -33,7 +15,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // pass
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // fail
-assertArraysEqual(['1', '2', '3'], ['1', '2', '3']); // pass
-assertArraysEqual(['1', '2', '3'], ['1', '2', 3]); // fail
+module.exports = assertArraysEqual;
