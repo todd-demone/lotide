3350a84e5cf007e18ea934eb3aeda03a77240c34
Todd Demone <tdemone1@gmail.com>
2021-11-10 15:42:38 +0000

    clean up assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,11 @@
 const eqArrays = require('./eqArrays');
 
-// Array, Array => no return value; side effect (console.log())
 const assertArraysEqual = function(actual, expected) {
   // Stringify the arrays so they look like arrays when printed to console.
   const actualStr = JSON.stringify(actual);
   const expectedStr = JSON.stringify(expected);
-  // compare actual and expected (both are arrays) to see if equal
-  // if result is true, print a pass message
   if (eqArrays(actual, expected)) {
     console.log(`✅️ Assertion Passed: ${actualStr} === ${expectedStr}`);
-  // else, print a fail message
   } else {
     console.log(`🛑️ Assertion Failed: ${actualStr} !== ${expectedStr}`);
   }
