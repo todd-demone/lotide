3a197d70838c12a533b31d5db0b436a25d4a1f22
Todd Demone <tdemone1@gmail.com>
2021-11-11 16:49:12 +0000

    fix typo in docstring

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,7 +3,7 @@ const eqArrays = require('./eqArrays');
 /**
  * Asserts equality of two arrays, `actual` and `expected`.
  * @param {Array} actual The array to compare.
- * @param {Array} expected The other array to compare
+ * @param {Array} expected The other array to compare.
  */
 const assertArraysEqual = function(actual, expected) {
   // Stringify the arrays so they look like arrays when printed to console.
